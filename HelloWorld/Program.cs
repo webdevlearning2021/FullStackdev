6b24178986a17b1fea24af490408e71e396fcf93
Shan <fall1975@ymail.com>
2021-07-17 19:34:06 -0400

    Day1 Helloworld app

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.WriteLine("Hello World!!!");   
+
+            Console.WriteLine("Enter your name");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello" + name);
+            Console.ReadKey();
+
+        }
+    }
+}
